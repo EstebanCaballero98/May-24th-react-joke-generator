492bfec38d85ab7030f2f6aba001314eba41128f
Esteban Cotton-Caballero <estebancotcab98@gmail.com>
2022-05-24 00:27:19 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [joke, changeJoke] = useState();
+  const [punchline, changePunchline] = useState();
+
+  const getJoke = async () => {
+    try {
+      const res = await fetch(" http://localhost:5000/jokes");
+      if (!res.ok) {
+        throw new Error(`http error ${res.status}`);
+      }
+      const data = await res.json();
+      const randomNumber = Math.floor(Math.random() * data.length);
+      changeJoke((prev) => data[randomNumber].setup);
+      changePunchline((prev) => data[randomNumber].punchline);
+    } catch (error) {
+      console.log(`error ${error}`);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <button onClick={getJoke} className="button">
+          Generate joke
+        </button>
+        <div className="qoute">{joke}</div>
+        <div className="author">{punchline}</div>;
+      </div>
     </div>
   );
 }
